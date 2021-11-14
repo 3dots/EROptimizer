a1c7407af5be8bd29bda66ff9342146cdf5960fa
Viktor Riabtsev <viktor.riabtsev@live.ca>
2021-11-14 17:22:44 -0500

    Starting scraping

---
+++ EROptimizer/Controllers/ScrapeWikiController.cs
@@ -0,0 +1,22 @@
+﻿using EROptimizer.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EROptimizer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScrapeWikiController
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ArmorChangedDto>> ScrapeWiki()
+        {
+
+
+            return new List<ArmorChangedDto>() { new ArmorChangedDto() { ArmorSet = "Test" } };
+        }
+    }
+}
