5f54504b5cf71fc8559d31c497eed78417c9ab38
Viktor Riabtsev <viktor.riabtsev@live.ca>
2021-11-19 17:59:46 -0500

    SPA WIP

--- EROptimizer/Startup.cs
+++ EROptimizer/Startup.cs
@@ -16,6 +16,11 @@ namespace EROptimizer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
